794c519a0b51b74a4c0ba82c500d5534c8558e01
Davin Lewis <102100872+davinlewis@users.noreply.github.com>
2024-02-18 01:21:12 -0800

    end transion

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -5,7 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
+    [SerializeField] AnimationClip clip;
     public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+    public void LoadSceneOnDelay(string sceneName)
+    {
+        StartCoroutine(DelayedTransition(clip.length, sceneName));
+    }
+
+    IEnumerator DelayedTransition(float time, string sceneName)
+    {
+        yield return new WaitForSeconds(time);
         SceneManager.LoadScene(sceneName);
     }
 }
