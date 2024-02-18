cddcfe59a0a7ff08ec11a86d073a9cbdf62300f1
Davin Lewis <102100872+davinlewis@users.noreply.github.com>
2024-02-18 02:08:19 -0800

    hehe

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] AnimationClip clip;
     public void LoadGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        animator.Play("MainMenuEyesClose");
+        StartCoroutine(GameStart(clip.length));
     }
 
     public void ExitGame()
     {
+        animator.Play("MainMenuEyesClose");
+        StartCoroutine(Close(clip.length));
+    }
+
+    IEnumerator GameStart(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    IEnumerator Close(float time)
+    {
+        yield return new WaitForSeconds(time);
         Application.Quit();
     }
 }
